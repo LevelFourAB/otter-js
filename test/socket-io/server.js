825a85630d1036612ccf3a96ec8190ee79f34442
Andreas Holstenson <a@holstenson.se>
2016-08-03 09:35:33 +0200

    Browser-based test page

--- test/socket-io/server.js
+++ test/socket-io/server.js
@@ -2,7 +2,6 @@
 
 const port = 7000;
 
-const io = require('socket.io')(port);
 console.log('starting at port', port);
 
 const SocketIoServer = require('../../src/engine/socket-io-server');
@@ -10,6 +9,35 @@ const InMemoryHistory = require('../../src/engine/in-memory-history');
 
 const combined = require('../../src/operations/combined');
 const type = combined.newType();
+
+const app = require('http').createServer(handler);
+const io = require('socket.io')(app);
+const fs = require('fs');
+
+app.listen(port);
+
+function handler(req, res) {
+	let filename;
+	if(req.url === '/') {
+		filename = __dirname + '/index.html';
+	} else if(req.url === '/otter.js') {
+		filename = __dirname + '/../../build/otter.js';
+	} else {
+		res.writeHead(404);
+		return res.end();
+	}
+
+	fs.readFile(filename, function(err, data) {
+		if(err) {
+			res.writeHead(500);
+			return res.end('Error loading');
+	    }
+
+		res.writeHead(200);
+		res.end(data);
+	});
+}
+
 
 const server = new SocketIoServer(io, {
 	history: function(id) {

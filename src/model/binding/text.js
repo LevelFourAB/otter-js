ae0b5ac1fa6be3cc04f5c67fbb1651a4bf514ce8
Andreas Holstenson <a@holstenson.se>
2016-08-21 20:39:05 +0200

    Updating text binding

--- src/model/binding/text.js
+++ src/model/binding/text.js
@@ -15,55 +15,74 @@ function bind(string, element) {
 	}
 
 	events.forEach(event => {
-		ignore = true;
-		try {
-			element.addEventListener(event, snapshot);
-		} finally {
-			ignore = false;
-		}
+		element.addEventListener(event, snapshot);
 	});
 
 	// TODO: Smarter way to track selection movememt
-	string.on('insert', function(e) {
+	function insertText(e) {
 		if(e.local) return;
 
-		let start = element.selectionStart;
-		let end = element.selectionEnd;
+		ignore = true;
+		try {
+			let start = element.selectionStart;
+			let end = element.selectionEnd;
 
-		const length = e.value.length;
+			const length = e.value.length;
 
-		element.value = element.value.substring(0, e.index) +
-			e.value +
-			element.value.substring(e.index);
+			element.value = element.value.substring(0, e.index) +
+				e.value +
+				element.value.substring(e.index);
 
-		// Transform the selection
-		if(start > e.index) start += length;
-		if(end > e.index) end += length;
+			// Transform the selection
+			if(start > e.index) start += length;
+			if(end > e.index) end += length;
 
-		element.selectionStart = start;
-		element.selectionEnd = end;
-	});
+			element.selectionStart = start;
+			element.selectionEnd = end;
+		} finally {
+			ignore = false;
+		}
+	}
 
-	string.on('delete', function(e) {
+	function deleteText(e) {
 		if(e.local) return;
 
-		let start = element.selectionStart;
-		let end = element.selectionEnd;
+		ignore = true;
+		try {
+			let start = element.selectionStart;
+			let end = element.selectionEnd;
 
-		const length = e.value.length;
+			const length = e.value.length;
 
-		element.value = element.value.substring(0, e.fromIndex) +
-			element.value.substring(e.toIndex);
+			element.value = element.value.substring(0, e.fromIndex) +
+				element.value.substring(e.toIndex);
 
-		// Transform the selection
-		if(start > e.fromIndex) start = Math.max(e.fromIndex, start - length);
-		if(end > e.fromIndex) end = Math.max(e.fromIndex, end - length);
+			// Transform the selection
+			if(start > e.fromIndex) start = Math.max(e.fromIndex, start - length);
+			if(end > e.fromIndex) end = Math.max(e.fromIndex, end - length);
 
-		element.selectionStart = start;
-		element.selectionEnd = end;
-	});
+			element.selectionStart = start;
+			element.selectionEnd = end;
+		} finally {
+			ignore = false;
+		}
+	}
+
+	string.on('insert', insertText);
+	string.on('delete', deleteText);
 
 	element.value = string.get();
+
+	return {
+		disconnect: function() {
+			string.removeEventListener('insert', insertText);
+			string.removeEventListener('delete', deleteText);
+
+			events.forEach(event => {
+				element.removeEventListener(event, snapshot);
+			});
+		}
+	};
 }
 
 module.exports = bind;
